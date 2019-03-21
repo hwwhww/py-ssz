bf36f1d98d73902a2686dc5a2e095bb978464eca
Jannik Luhn <jannik@brainbot.com>
2019-03-21 20:18:54 +0100

    Implement utilities for tree hashing

--- ssz/constants.py
+++ ssz/constants.py
@@ -1,5 +1,9 @@
+from eth_typing import (
+    Hash32,
+)
+
 CHUNK_SIZE = 32
-EMPTY_CHUNK = b"\x00" * CHUNK_SIZE
+EMPTY_CHUNK = Hash32(b"\x00" * CHUNK_SIZE)
 
 SIZE_PREFIX_SIZE = 4
 MAX_CONTENT_SIZE = 2 ** (SIZE_PREFIX_SIZE * 8) - 1
